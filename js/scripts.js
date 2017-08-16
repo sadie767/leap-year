be918efcf32d548b225639b06d95b40fe3030570
Sarah Lindmar <srhcrete@gmail.com>
2017-08-15 21:51:39 -0700

    add last spec for divisible by 400 to leap year spec

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
       $("#result").text(result);
   });
   var leapYear = function(year) {
-    if ((year % 4 === 0) && (year % 100 !==0)) {
+    if ((year % 4 === 0) && (year % 100 !==0) || (year % 400 === 0)) {
       return true;
     } else {
       return false;
