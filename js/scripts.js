994d12465f96d3193590f34ebfd0a41eb8dd4ac5
Sarah Lindmar <srhcrete@gmail.com>
2017-08-15 21:48:48 -0700

    refactor divisicle by 4 and divisible by 100 rules to same statement of leap year function

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,7 @@ $(document).ready(function() {
       $("#result").text(result);
   });
   var leapYear = function(year) {
-    if (year % 100 === 0){
-      return false;
-    } else if (year % 4 === 0) {
+    if ((year % 4 === 0) && (year % 100 !==0)) {
       return true;
     } else {
       return false;
